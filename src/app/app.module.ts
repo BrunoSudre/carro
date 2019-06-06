26e46c7b232f92462ba9f696e24538b8cf4664cd
Bruno <bruninhosudre@gmail.com>
2019-06-06 04:21:02 -0300

    finalização do formulário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PainelComponent } from './painel/painel.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PainelComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
